b3f0ab3964bff7bd3ff810610ff31492138e6257
Quan Do <biquando@gmail.com>
2023-11-06 20:43:28 -0800

    Add support for lists and quotes

--- main.ts
+++ main.ts
@@ -11,6 +11,34 @@ const DEFAULT_SETTINGS: SoftBreaksSettings = {
 export default class SoftBreaks extends Plugin {
     settings: SoftBreaksSettings;
 
+    isSpace(ch: string): boolean {
+        return ch === ' ' || ch === '\t';
+    }
+
+    isNotSpace(ch: string): boolean {
+        return !(ch === ' ' || ch === '\t');
+    }
+
+    nextCh(pred: (t: string) => boolean, text: string, idx: number): number {
+        if (idx < 0) {
+            return text.length;
+        }
+        for (let i = idx; i < text.length; i++) {
+            if (pred(text[i])) return i;
+        }
+        return text.length;
+    }
+
+    prevCh(pred: (t: string) => boolean, text: string, idx: number): number {
+        if (idx >= text.length) {
+            return -1;
+        }
+        for (let i = idx; i >= 0; i--) {
+            if (pred(text[i])) return i;
+        }
+        return -1;
+    }
+
     async onload() {
         await this.loadSettings();
 
@@ -28,75 +56,78 @@ export default class SoftBreaks extends Plugin {
                 }
                 let inCodeBlock = false;
                 for (let l = 0; l < editor.lineCount(); l++) {
-                	let line = editor.getLine(l);
+                    const rawLine = editor.getLine(l);
+                    let line = rawLine;
+                    if (line.length <= COL) {
+                        continue;
+                    }
 
                     // check for ``` to change inCodeBlock
-                    if (/\s*```.*/.test(line)) {
+                    if (/^\s*```/.test(line)) {
                         inCodeBlock = !inCodeBlock;
                     }
-                    if (inCodeBlock) continue;
+                    if (inCodeBlock) {
+                        continue;
+                    }
 
-                	if (line.length > COL) {
-                        if ((line[0] === '-' && line[1] === ' ')
-                            || line[0] === '\t' || line[0] === ' '
-                            || (line[0] >= '0' && line[0] <= '9' && line[1] === '.')
-                            || (line[0] === '>' && line[1] === ' ')
-                        ) {
-                            continue; // don't change these lines
+                    let offset = "";
+                    do {
+                        // unordered lists
+                        const ulistMatches = line.match(/^(\t*|[ ]*)-[ ]+/);
+                        if (ulistMatches) {
+                            offset = ulistMatches[0].replace(/[^\t ]/g, ' ');
+                            break;
                         }
 
-                        let overflowWordBeg = -1;
-                        if (line[COL] === ' ') { // if COL is a space
-                            for (let i = COL + 1; i < line.length; i++) {
-                                if (line[i] !== ' ') {
-                                    overflowWordBeg = i; // COL is a space, so find the next word
-                                    break;
-                                }
-                            }
-                            if (overflowWordBeg === -1) overflowWordBeg = line.length; // line ends in whitespace
-                        } else { // if COL is not a space
-                            for (let i = COL - 1; i >= 0; i--) {
-                                if (line[i] === ' ') {
-                                    overflowWordBeg = i + 1; // find the beginning of the word crossing COL
-                                    break;
-                                }
-                            }
-                            if (overflowWordBeg == -1) { // if COL is inside a really long word that takes the whole line
-                                let nextSpace = -1;
-                                for (let i = COL + 1; i < line.length; i++) {
-                                    if (line[i] === ' ') {
-                                        nextSpace = i; // if cols 0..COL are non-spaces, then go to the next space after COL
-                                        break;
-                                    }
-                                }
-                                if (nextSpace === -1) continue; // the whole line is a single word
-                                for (let i = nextSpace + 1; i < line.length; i++) {
-                                    if (line[i] !== ' ') {
-                                        overflowWordBeg = i; // find the next word after COL
-                                        break;
-                                    }
-                                }
-                            }
-                            if (overflowWordBeg < 0) continue;
+                        // ordered lists
+                        const olistMatches = line.match(/^(\t*|[ ]*)\d+\.[ ]+/);
+                        if (olistMatches) {
+                            offset = olistMatches[0].replace(/[^\t ]/g, ' ');
+                            break;
                         }
 
-                        let lastWordEnd = -1;
-                        for (let i = overflowWordBeg - 1; i >= 0; i--) {
-                            if (line[i] !== ' ') {
-                                lastWordEnd = i;
-                                break;
-                            }
+                        // quotes
+                        const quoteMatches = line.match(/^(\t*|[ ]*)>[ ]+/)
+                        if (quoteMatches) {
+                            offset = quoteMatches[0].replace(/[^\t ]/g, ' ');
+                            break;
                         }
-                        if (lastWordEnd === -1) continue;
 
-                		line = line.slice(0, lastWordEnd + 1) + '\n' + line.slice(overflowWordBeg);
-                		editor.setLine(l, line);
-                	}
+                        // indentation
+                        const indentMatches = line.match(/^\t*[ ]+/);
+                        if (indentMatches) {
+                            offset = indentMatches[0];
+                            break;
+                        }
+                    } while (0);
+                    line = offset + line.slice(offset.length);
+
+                    let overflowWordBeg;
+                    if (line[COL] === ' ') { // if COL is a space
+                        overflowWordBeg = this.nextCh(this.isNotSpace, line, COL + 1);
+                    } else { // if COL is not a space
+                        overflowWordBeg = 1 + this.prevCh(this.isSpace, line, COL - 1);
+
+                        if (overflowWordBeg === 0) { // if COL is inside a really long word that takes the whole line
+                            let nextSpace = this.nextCh(this.isSpace, line, COL + 1);
+                            if (nextSpace === line.length) {
+                                continue; // the whole line is a single word
+                            }
+                            overflowWordBeg = this.nextCh(this.isNotSpace, line, nextSpace + 1);
+                        }
+                    }
+
+                    let lastWordEnd = this.prevCh(this.isNotSpace, line, overflowWordBeg - 1);
+                    if (lastWordEnd === -1) {
+                        continue;
+                    }
+
+                    line = rawLine.slice(0, lastWordEnd + 1) + '\n' + offset + line.slice(overflowWordBeg);
+                    editor.setLine(l, line);
                 }
             }
         });
 
-        // This adds a settings tab so the user can configure various aspects of the plugin
         this.addSettingTab(new SoftBreaksSettingTab(this.app, this));
     }
 
